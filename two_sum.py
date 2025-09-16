09bc746b8061a50373e2abcb8787d4ea30dd5bcb
Robbiesactforthehub <03robert.strongmorse@kamsc.org>
2025-09-16 11:49:33 -0400

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -4,7 +4,8 @@ def two_sum_pairs(numbers, target):
         for r in range(0,n):
             if(target-numbers[n]==numbers[r]):
                 if (n!=r):
-                     list.insert(0,{numbers[r],numbers[n]})   
+                     list.append({numbers[r],numbers[n]})
+                     reversed(list)   
     return list
 
 
